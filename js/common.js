c7f0cffa28cecaf63d9fef286deea784dd1a32ac
hraboviyvadim <hraboviyvadim@gmail.com>
2013-08-15 18:06:19 +0300

    torchoo.html added

--- js/common.js
+++ js/common.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
 
+	$( ".js__draggable" ).draggable({ 
+		grid: [ 10, 10 ],
+		cursor: "move", 
+		revert: "invalid" 
+	});
+	$( ".currencies__droppable" ).droppable({
+	  activeClass: "ui-state-hover",
+      hoverClass: "ui-state-active",
+      drop: function( event, ui ) {
+        $( this )
+          .addClass( "ui-state-highlight" )
+          .find( "p" )
+            .html( "Dropped!" );
+      }
+    });
 });
