61082b783d6f67bdc2c22fd693cf2c806478d769
akella <akella@Yuris-MacBook-Pro.local>
2013-11-23 10:03:57 +0200

    torchoo

--- js/common.js
+++ js/common.js
@@ -1,27 +1,3 @@
 $(document).ready(function() {
 
-	$( ".js__draggable" ).draggable({ 
-		grid: [ 10, 10 ],
-		cursor: "move", 
-		revert: "invalid" 
-	});
-	$( ".currencies__droppable" ).droppable({
-	  activeClass: "ui-state-hover",
-      hoverClass: "ui-state-active",
-      drop: function( event, ui ) {
-        $( this )
-          .addClass( "ui-state-highlight" )
-          .find( "p" )
-            .html( "Dropped!" );
-      }
-    });
-
-    $(".add-record-btn").click(function(){
-
-      $(this).hide();
-      $(".wrapper-popup").show();
-
-    })
-
-
 });
